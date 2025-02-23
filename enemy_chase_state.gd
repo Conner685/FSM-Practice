b57b995b2a5651b37337c00928893d3cb5cc5756
Conner <cponton@my.bcit.ca>
2025-02-23 15:57:48 -0800

    Update enemy_chase_state.gd

--- enemy_chase_state.gd
+++ enemy_chase_state.gd
@@ -18,10 +18,13 @@ func _exit_state() -> void:
 	set_physics_process(false)
 	
 func _physics_process(delta) -> void:
+
 	animator.scale.x = -sign(actor.velocity.x)
+
 	if animator.scale.x == 0.0: animator.scale.x = 1.0
 	var direction = Vector2.ZERO.direction_to(actor.get_local_mouse_position())
 	actor.velocity = actor.velocity.move_toward(direction * actor.max_speed, actor.acceleration * delta)
 	actor.move_and_slide()
+
 	if vision_cast.is_colliding():
 		lost_player.emit()
